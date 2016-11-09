7244b7a828f10c9a1d85d6818aaa2efe3c32f4e2
murarivarma <murarivarma@hotmail.com>
2016-11-09 11:33:04 +0530

    Displaying the data received in home from register.

--- app/home/homeCtrl.js
+++ app/home/homeCtrl.js
@@ -2,6 +2,7 @@
     function homeCtrlFunction($scope, $state){
         //console.log($state);
         $scope.name = "Mr";
+        $scope.userData = $state.params.homeData;
     }
     
     angular.module("home").controller("homeCtrl", ["$scope", "$state", homeCtrlFunction]);
